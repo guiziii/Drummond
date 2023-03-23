343ac8a5ac6b6707e237a794e322ebc60b9162e1
Guilherme <zuurc@hotmail.com>
2023-03-23 03:39:37 -0300

    feat(i18n/metrics): Added multiple languages and media port util.

---
+++ packages/web/src/i18n/languages.ts
@@ -0,0 +1,9 @@
+import brazilIcon from "@drummond-advisors/shared/assets/brazil.png";
+import usaIcon from "@drummond-advisors/shared/assets/usa.png";
+
+const languages = [
+  { key: 1, label: "en", icon: usaIcon },
+  { key: 2, label: "pt", icon: brazilIcon },
+];
+
+export default languages;
---
+++ packages/web/src/metrics/index.ts
@@ -0,0 +1,13 @@
+const MediaQuery = {
+  ExtraSmallDevices: "@media only screen and (max-width: 600px)",
+  SmallDevices: "@media only screen and (min-width: 600px)",
+  MediumDevices: "@media only screen and (min-width: 768px) ",
+  LargeDevices: "@media only screen and (min-width: 992px)",
+  ExtraLargeDevices: "@media only screen and (min-width: 1200px)",
+};
+
+const Metrics = {
+  MediaQuery,
+};
+
+export default Metrics;
