75f22e1c12ba68f6a67ee13ae8024a3f663fd427
Guilherme <zuurc@hotmail.com>
2023-03-23 03:40:17 -0300

    feat(firebase): Added firebase config.

---
+++ packages/web/src/firebase/config/index.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDceH9amyB2Var1mFx0GqMO1o1bt7E_fbY",
+  authDomain: "drummond-e8706.firebaseapp.com",
+  projectId: "drummond-e8706",
+  storageBucket: "drummond-e8706.appspot.com",
+  messagingSenderId: "279886335338",
+  appId: "1:279886335338:web:d220d616dc14bdc2ac6913",
+  measurementId: "G-N8LKHKK1NE",
+};
+
+export default firebaseConfig;
