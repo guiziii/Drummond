78af4a746b6d48737917a6b0f1d937f61a3266ef
Guilherme <zuurc@hotmail.com>
2023-03-23 23:00:28 -0300

    fix(api/main.ts): Updated port

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -20,6 +20,6 @@ export default async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
