c5c08dcb7dff9d7f2a416e66353e8a57d5ec36aa
Guilherme Cruz <zuurc@hotmail.com>
2023-03-24 00:59:19 -0300

    fix(env): Added port

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -20,6 +20,6 @@ export default async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8089);
+  await app.listen(process.env.PORT || 8089);
 }
 bootstrap();
